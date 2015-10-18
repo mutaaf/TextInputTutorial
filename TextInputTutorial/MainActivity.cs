b83299b6b8ab74e6cb1c23cab2911476d23a8424
Mutaaf Aziz <mutaaf.aziz@gmail.com>
2015-10-18 16:42:17 -0500

    Initial Commit

---
+++ TextInputTutorial/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace TextInputTutorial
+{
+	[Activity (Label = "TextInputTutorial", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get TextView and EditText
+			var editText = FindViewById<EditText> (Resource.Id.txtEditText);
+			var textView = FindViewById<TextView> (Resource.Id.lblTextView);
+
+			// Get the value of editText
+			var userInput = editText.Text;
+
+			// Add event handler for text change
+			editText.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) => 
+			{
+				textView.Text = e.Text.ToString();
+			}; 
+
+		}
+	}
+}
+
+
